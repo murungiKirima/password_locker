2906a0daa346374aa9e6d0ab43d1842a70ea0a03
murungi kirima <murungikirima@gmail.com>
2018-05-04 16:45:20 +0300

    Add Credentials class

--- account.py
+++ account.py
@@ -15,3 +15,24 @@ class User:
     @classmethod
     def display_user(self):
         return cls.user_list
+
+class Credentials:
+    credentials_list = []
+
+    def __init__(self,f_name,l_name,email,p_number,proffession,hobby):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.email = email
+        self.p_number = p_number
+        self.proffession = proffession
+        self.hobby = hobby
+
+    def save_credentials(self):
+        Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def display_credentials(self):
+        return cls.credentials_list
